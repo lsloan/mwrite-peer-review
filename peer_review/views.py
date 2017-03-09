468db7c9f9fe51423cac3749d04fb4c0145b68b3
Dave Harlan <dkharlan@umich.edu>
2017-03-09 11:32:26 -0500

    Add LTI launch params to index page for debugging

--- peer_review/views.py
+++ peer_review/views.py
@@ -41,6 +41,11 @@ class LtiProxyView(FixedHttpProxy, LtiView):
 class IndexView(LtiView, TemplateView):
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['launch_params'] = self.request.session['lti_launch_params']
+        return context
+
 
 class UnauthorizedView(TemplateView):
     template_name = '403.html'
