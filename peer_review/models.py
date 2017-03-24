233314082556cab2adedb24f0bb4cbc188f7f175
Dave Harlan <dkharlan@umich.edu>
2017-03-24 00:05:13 -0400

    Add unmanaged model for canvas_assignments

--- peer_review/models.py
+++ peer_review/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class CanvasAssignments(models.Model):
+
+    class Meta:
+        managed = False
+        db_table = 'canvas_assignments'
+
+    id = models.AutoField(primary_key=True)
+    title = models.TextField()
+    course_id = models.IntegerField()
+    due_date_utc = models.DateTimeField()
+    is_peer_review_assignment = models.BooleanField()
