acdc7f74dc2668aef1deb560d42b63ace31812a4
Dave Harlan <dkharlan@umich.edu>
2017-04-03 15:25:40 -0400

    Add Safari iframe landing page and render it in the safari_iframe_launch_middleware

--- peer_review/middleware.py
+++ peer_review/middleware.py
@@ -1,5 +1,6 @@
 import logging
 from django.conf import settings
+from django.shortcuts import render_to_response
 from django.utils.deprecation import MiddlewareMixin
 from django_user_agents.middleware import UserAgentMiddleware
 
@@ -13,21 +14,19 @@ class FixedUserAgentMiddleware(MiddlewareMixin, UserAgentMiddleware):
 
 def safari_iframe_launch_middleware(get_response):
     def middleware(request):
-
         logger.debug('(safari iframe) request.path = %s' % request.path)
         logger.debug('(safari iframe) request.user_agent.browser.family = %s' % request.user_agent.browser.family)
         logger.debug('(safari iframe) request.COOKIES = %s' % request.COOKIES)
         logger.debug('(safari iframe) request.META = %s' % request.META)
         logger.debug('(safari iframe) request.META[\'HTTP_REFERER\'] header = %s' % request.META['HTTP_REFERER'])
 
-        safari_first_launch = request.path == '/launch' and \
-                              request.user_agent.browser.family == 'Safari' and \
-                              settings.SESSION_COOKIE_NAME not in request.COOKIES
-
-        if safari_first_launch:
+        if request.path == '/launch' and \
+           request.user_agent.browser.family == 'Safari' and \
+           settings.SESSION_COOKIE_NAME not in request.COOKIES:
             logger.debug('safari first launch')
+            return render_to_response('safari_launch_iframe.html', {'referer': request.META['HTTP_REFERER']})
+        else:
+            response = get_response(request)
+        return response
 
-
-
-        return get_response(request)
     return middleware
