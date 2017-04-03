5f34d96dc15578681f770e288bbd92155305cbed
Dave Harlan <dkharlan@umich.edu>
2017-04-03 14:30:59 -0400

    Add some debug logging for safari_iframe_launch_middleware

--- peer_review/middleware.py
+++ peer_review/middleware.py
@@ -1,6 +1,9 @@
+import logging
 from django.conf import settings
 from django.utils.deprecation import MiddlewareMixin
 from django_user_agents.middleware import UserAgentMiddleware
+
+logger = logging.getLogger(__name__)
 
 
 # TODO remove when django-user-agents fixes https://github.com/selwin/django-user_agents/issues/13
@@ -11,12 +14,16 @@ class FixedUserAgentMiddleware(MiddlewareMixin, UserAgentMiddleware):
 def safari_iframe_launch_middleware(get_response):
     def middleware(request):
 
-        safari_first_launch = request.path == '/' and \
+        logger.debug('(safari iframe) request.path = %s' % request.path)
+        logger.debug('(safari iframe) request.user_agent.browser.family = %s' % request.user_agent.browser.family)
+        logger.debug('(safari iframe) request.COOKIES = %s' % request.COOKIES)
+
+        safari_first_launch = request.path == '/launch' and \
                               request.user_agent.browser.family == 'Safari' and \
                               settings.SESSION_COOKIE_NAME not in request.COOKIES
 
         if safari_first_launch:
-            print('safari first launch')
+            logger.debug('safari first launch')
 
         return get_response(request)
     return middleware
