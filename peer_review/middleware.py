8c90e64cabed0f1ec2293f054000ee68bcbb6151
Dave Harlan <dkharlan@umich.edu>
2017-04-04 10:55:51 -0400

    Remove most debug logging from safari_iframe_launch_middleware

--- peer_review/middleware.py
+++ peer_review/middleware.py
@@ -14,19 +14,12 @@ class FixedUserAgentMiddleware(MiddlewareMixin, UserAgentMiddleware):
 
 def safari_iframe_launch_middleware(get_response):
     def middleware(request):
-        logger.debug('(safari iframe) request.path = %s' % request.path)
-        logger.debug('(safari iframe) request.user_agent.browser.family = %s' % request.user_agent.browser.family)
-        logger.debug('(safari iframe) request.COOKIES = %s' % request.COOKIES)
-        logger.debug('(safari iframe) request.META = %s' % request.META)
-        logger.debug('(safari iframe) request.META[\'HTTP_REFERER\'] header = %s' % request.META['HTTP_REFERER'])
-
         if request.path == '/launch' and \
            request.user_agent.browser.family == 'Safari' and \
            settings.SAFARI_LAUNCH_COOKIE not in request.COOKIES:
-            logger.debug('safari first launch')
+            logger.debug('Unauthenticated Safari user detected, serving Safari landing page')
             return render_to_response('safari_launch_iframe.html', {'referer': request.META['HTTP_REFERER']})
         else:
             response = get_response(request)
         return response
-
     return middleware
