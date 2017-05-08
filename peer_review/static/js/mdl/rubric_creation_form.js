773a54ad7ae20193b21219d47bea5f4f40885928
Dave Harlan <dkharlan@umich.edu>
2017-05-08 10:16:16 -0400

    Implement criterion card delete button and remove it from the tab order

--- peer_review/static/js/mdl/rubric_creation_form.js
+++ peer_review/static/js/mdl/rubric_creation_form.js
@@ -1,4 +1,8 @@
 (function() {
+    window.removeCriterionCard = function(button) {
+        $(button).closest('.criterion-card').remove()
+    };
+
     $(document).ready(function () {
         autosize($('textarea'));
     });
