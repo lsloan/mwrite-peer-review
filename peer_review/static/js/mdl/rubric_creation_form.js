fae02d02e9ccdd26562dc63e249f3bbc3d014e30
Dave Harlan <dkharlan@umich.edu>
2017-05-08 11:14:52 -0400

    Implement addition of criterion cards

--- peer_review/static/js/mdl/rubric_creation_form.js
+++ peer_review/static/js/mdl/rubric_creation_form.js
@@ -1,6 +1,13 @@
 (function() {
     window.removeCriterionCard = function(button) {
-        $(button).closest('.criterion-card').remove()
+        $(button).closest('.criterion-card').remove();
+    };
+
+    window.addCriterionCard = function(template) {
+        var $newCard = $(template);
+        var textField = $newCard.find('.mdl-textfield').get(0);
+        componentHandler.upgradeElement(textField);
+        $('.criteria-container').append($newCard);
     };
 
     $(document).ready(function () {
