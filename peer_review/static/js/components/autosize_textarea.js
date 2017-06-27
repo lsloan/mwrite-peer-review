faeaac742110fa6a59770104ea1de0c2b0f475b6
Dave Harlan <dkharlan@umich.edu>
2017-06-27 16:01:09 -0400

    Modify AutosizeTextarea component to use _uid as the id attributes for the form element and label

--- peer_review/static/js/components/autosize_textarea.js
+++ peer_review/static/js/components/autosize_textarea.js
@@ -6,8 +6,8 @@ var AutosizeTextarea = {
         'mdl-textfield': VueMdl.components.MdlTextfield
     },
     template: '<div class="mdl-textfield mdl-js-textfield">' +
-                  '<textarea class="mdl-textfield__input" type="text" rows="1" @input="$emit(\'input\', $event.target.value)">{{ value }}</textarea>' +
-                  '<label for="rubric-description-textfield" class="mdl-textfield__label">{{ label }}</label>' +
+                  '<textarea :id="_uid" class="mdl-textfield__input" type="text" rows="1" @input="$emit(\'input\', $event.target.value)">{{ value }}</textarea>' +
+                  '<label :for="_uid" class="mdl-textfield__label">{{ label }}</label>' +
               '</div>',
     mounted: function() {
         autosize(this.$el.querySelector('textarea'));
