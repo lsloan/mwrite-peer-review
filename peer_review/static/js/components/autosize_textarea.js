c38257198c5ab9385fb7c3847f1d49126927f668
Dave Harlan <dkharlan@umich.edu>
2017-06-29 11:09:49 -0400

    Remove unneeded mdl-textfield component from components key of AutosizeTextarea

--- peer_review/static/js/components/autosize_textarea.js
+++ peer_review/static/js/components/autosize_textarea.js
@@ -2,9 +2,6 @@
 
 var AutosizeTextarea = {
     props: ['value', 'label'],
-    components: {
-        'mdl-textfield': VueMdl.components.MdlTextfield
-    },
     template: '<div class="mdl-textfield mdl-js-textfield">' +
                   '<textarea :id="_uid" class="mdl-textfield__input" type="text" rows="1" @input="$emit(\'input\', $event.target.value)">{{ value }}</textarea>' +
                   '<label :for="_uid" class="mdl-textfield__label">{{ label }}</label>' +
