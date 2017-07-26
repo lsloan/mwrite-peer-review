b239b34bf62e29c6e153733771945a5023e86a59
Dave Harlan <dkharlan@umich.edu>
2017-07-26 10:09:28 -0400

    Implement submission file uploads

--- peer_review/canvas.py
+++ peer_review/canvas.py
@@ -1,16 +1,23 @@
 import re
 import requests
+import mimetypes
+from io import SEEK_SET, SEEK_END
 from toolz.dicttoolz import merge
 from urllib.parse import urljoin
 from django.conf import settings
 
+# TODO rethink the token state flow???
+
 _page_regex = re.compile('<(?P<page_url>.*)>.*rel="(?P<page_key>.*)"')
+
+# TODO would be nice to have a mechanism for declaring with HTTP verbs each resource supports
 _routes = {
     'course':               {'route': 'courses/%s'},
     'assignments':          {'route': 'courses/%s/assignments'},
     'assignment':           {'route': 'courses/%s/assignments/%s'},
     'assignment-overrides': {'route': 'courses/%s/assignments/%s/overrides'},
     'submissions':          {'route': 'courses/%s/assignments/%s/submissions'},
+    'submission_file':      {'route': 'courses/%s/assignments/%s/submissions/self/files'},
     'students':             {'route': 'courses/%s/users',
                              'params': {
                                  'enrollment_type[]': ['student'],
@@ -73,4 +80,41 @@ def create(resource, *params, **kwargs):
                              json=kwargs['data'],
                              headers=_make_headers())
     response.raise_for_status()
-    return response
+    return response.json()
+
+
+def upload_submission(user_token, course_id, assignment_id, filename, contents, mime_type=None):
+
+    # TODO don't do this
+    saved_token = settings.CANVAS_API_TOKEN
+    settings.CANVAS_API_TOKEN = user_token
+
+    if not mime_type:
+        mime_type, _ = mimetypes.guess_type(filename)
+        if not mime_type:
+            raise RuntimeError('Unknown MIME type for %s' % filename)
+
+    contents.seek(0, SEEK_END)
+    file_size = contents.tell()
+    contents.seek(0, SEEK_SET)
+
+    file_desc = {
+        'name': filename,
+        'size': file_size,
+        'content_type': mime_type
+    }
+    pending_file_desc = create('submission_file', course_id, assignment_id, data=file_desc)
+
+    file_upload_response = requests.post(pending_file_desc['upload_url'],
+                                         data=pending_file_desc['upload_params'],
+                                         files={'file': contents},
+                                         allow_redirects=False)
+    file_upload_response.raise_for_status()
+
+    file_confirmation_response = requests.post(file_upload_response.headers['location'])
+    file_confirmation_response.raise_for_status()
+
+    # TODO remove this when we fix the above
+    settings.CANVAS_API_TOKEN = saved_token
+
+    return file_confirmation_response.json()
