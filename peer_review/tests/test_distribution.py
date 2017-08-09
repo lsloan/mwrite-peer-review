c4ebe782f3749476db9121a83053a96ebe24f529
Dave Harlan <dkharlan@umich.edu>
2017-08-09 14:57:29 -0400

    Upgrade Hypothesis to latest version; add rubric and associated assignments generation

--- peer_review/tests/test_distribution.py
+++ peer_review/tests/test_distribution.py
@@ -1,12 +1,14 @@
 import string
 from functools import partial
+from datetime import datetime
 from statistics import mean, stdev
 
 from hypothesis import given
-from hypothesis.strategies import composite, sets, integers, lists, just, text
 from hypothesis.extra.django.models import models
+from hypothesis.strategies import composite, sets, integers, lists, just, text
+from dateutil.relativedelta import relativedelta
 
-from peer_review.models import CanvasStudent, CanvasSubmission, CanvasCourse, CanvasSection
+from peer_review.models import CanvasAssignment, CanvasStudent, CanvasSubmission, CanvasCourse, CanvasSection, Rubric
 from peer_review.distribution import make_distribution
 
 
@@ -42,6 +44,63 @@ def name():
     return text(alphabet=alphabet, min_size=3, max_size=15)
 
 
+@composite
+def canvas_assignment(draw, course_strategy, due_date_utc_strategy, is_peer_review_assignment_strategy):
+    _course = draw(course_strategy)
+    _due_date_utc = draw(due_date_utc_strategy)
+    _is_peer_review_assignment = draw(is_peer_review_assignment_strategy)
+    _id = draw(db_id())
+    _title = draw(name())
+    assignment = CanvasAssignment(id=_id,
+                                  title=_title,
+                                  course=_course,
+                                  due_date_utc=_due_date_utc,
+                                  is_peer_review_assignment=_is_peer_review_assignment)
+    assignment.save()
+    return assignment
+
+
+def _add_sections_to_rubric(sections, _rubric):
+    for section in sections:
+        _rubric.sections.add(section)
+    return just(_rubric)
+
+
+@composite
+def rubric(draw, prompt_strategy, peer_review_strategy, sections):
+    _peer_review = draw(peer_review_strategy)
+    _rubric = Rubric(id=draw(db_id()),
+                     description=draw(name()),
+                     reviewed_assignment=draw(prompt_strategy),
+                     passback_assignment=_peer_review,
+                     peer_review_open_date=_peer_review.due_date_utc,
+                     distribute_peer_reviews_for_sections=True)
+    _rubric.save()
+    for section in sections:
+        _rubric.sections.add(section)
+    return _rubric
+
+
+@composite
+def complete_rubric(draw, course):
+
+    now = datetime.utcnow()
+    five_minutes = relativedelta(minutes=5)
+    five_minutes_ago = now - five_minutes
+    five_minutes_from_now = now + five_minutes
+
+    just_course = just(course)
+
+    prompt = draw(canvas_assignment(just_course,
+                                    just(five_minutes_ago),
+                                    just(False)))
+    peer_review = draw(canvas_assignment(just_course,
+                                         just(five_minutes_from_now),
+                                         just(True)))
+
+    return draw(rubric(just(prompt), just(peer_review), course.sections.all()))
+
+
 def canvas_section(course):
     return models(CanvasSection, id=db_id(), name=name(), course=just(course))
 
@@ -66,7 +125,7 @@ def generate_students_for_section(section):
 
 def generate_sections_for_course(course):
     section = canvas_section(course).flatmap(generate_students_for_section)
-    return lists(section, min_size=1, max_size=10).map(lambda _: course)
+    return lists(section, min_size=2, max_size=10).map(lambda _: course)
 
 
 def canvas_course():
