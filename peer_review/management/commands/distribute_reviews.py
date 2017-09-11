5b4b7091b3771d6be86c70c8bb288203328d6cab
Dave Harlan <dkharlan@umich.edu>
2017-09-11 14:14:20 -0400

    Add some better exception logging for review distribution management command

--- peer_review/management/commands/distribute_reviews.py
+++ peer_review/management/commands/distribute_reviews.py
@@ -1,7 +1,10 @@
+import logging
 from datetime import datetime
 from dateutil.tz import tzutc
 from django.core.management import BaseCommand, CommandError
 from peer_review.distribution import review_distribution_task
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -11,4 +14,5 @@ class Command(BaseCommand):
         try:
             review_distribution_task(datetime.now(tzutc()))
         except Exception as ex:
+            logger.exception('Uncaught exception when running review distribution task')
             raise CommandError('Failed to start review distribution') from ex
