027e52cec2e39302eaf72427997384f8320c5ce1
Samantha Silveira <silvesam@umich.edu>
2018-01-25 14:57:18 -0500

    Add basic RouteGuard component in order to play with route guards

---
+++ frontend/src/components/RouteGuard.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>Try out auth</div>
+</template>
+<script type="text/javascript">
+
+</script>
+<style scoped>
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+import RouteGuard from '@/components/RouteGuard';
 
 Vue.use(Router);
+
+const authFunc = (to, from, next) => {
+  console.log('nope');
+  next(false);
+};
 
 export default new Router({
   routes: [
@@ -10,6 +16,12 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/tryAuth',
+      name: 'AuthComponent',
+      component: RouteGuard,
+      beforeEnter: authFunc
     }
   ]
 });
