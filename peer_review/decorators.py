01be864ee5c5d2ee1e9d1932f76ef1bcdb076dbd
Dave Harlan <dkharlan@umich.edu>
2018-02-01 10:55:26 -0500

    Don't try to keymap dicts

--- peer_review/decorators.py
+++ peer_review/decorators.py
@@ -47,10 +47,14 @@ def json_response(view):
 
     def wrapper(*args, **kwargs):
         data = view(*args, **kwargs)
-        if isinstance(data, Iterable):
+        is_iterable = isinstance(data, Iterable)
+        is_dict = isinstance(data, dict)
+
+        if is_iterable and not is_dict:
             content = list(map(camel_caser, data))
         else:
             content = camel_caser(data)
+
         return HttpResponse(content=json.dumps(content),
                             content_type='application/json')
     return wrapper
