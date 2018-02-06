ce8d74643573ac0b4bf14dd38647661272e44583
Dave Harlan <dkharlan@umich.edu>
2018-02-06 14:25:12 -0500

    Reorder endpoints

--- peer_review/api/endpoints.py
+++ peer_review/api/endpoints.py
@@ -8,12 +8,6 @@ from peer_review.decorators import authorized_json_endpoint, authenticated_json_
 logger = logging.getLogger(__name__)
 
 
-@authorized_json_endpoint(roles=['instructor'])
-def all_students(request, course_id):
-    students = CanvasStudent.objects.filter(course_id=course_id)
-    return [student.to_dict(levels=2) for student in students]
-
-
 @authenticated_json_endpoint
 def logged_in_user_details(request):
     roles = [role.get_name() for role in get_user_roles(request.user)]
@@ -23,3 +17,9 @@ def logged_in_user_details(request):
         'course_id': course_id,
         'roles': roles
     }
+
+
+@authorized_json_endpoint(roles=['instructor'])
+def all_students(request, course_id):
+    students = CanvasStudent.objects.filter(course_id=course_id)
+    return [student.to_dict(levels=2) for student in students]
