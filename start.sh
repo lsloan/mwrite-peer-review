0189b4119ec24c1604dca6d75fe6e294349a9317
Dave Harlan <dkharlan@umich.edu>
2018-02-09 13:42:20 -0500

    Add GUNICORN_WORKER_TIMEOUT environment variable for setting the gunicorn worker timeout (in seconds; defaults to 30)

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 GUNICORN_WORKERS="${GUNICORN_WORKERS:-4}"
 GUNICORN_PORT=8000
+GUNICORN_WORKER_TIMEOUT=${GUNICORN_WORKER_TIMEOUT-30}
 
 printenv >> /etc/environment
 
@@ -10,6 +11,7 @@ set -x
 cron
 
 gunicorn \
-    --workers="$GUNICORN_WORKERS" \
-    --bind=0.0.0.0:"$GUNICORN_PORT" \
+    --workers="$GUNICORN_WORKERS"        \
+    --bind=0.0.0.0:"$GUNICORN_PORT"      \
+    --timeout="$GUNICORN_WORKER_TIMEOUT" \
     mwrite_peer_review.wsgi:application
