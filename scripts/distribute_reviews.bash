cc50ee509a949238fda96a3f86f0e553e31f9af1
Dave Harlan <dkharlan@umich.edu>
2018-03-09 10:44:19 -0500

    Correct DJANGO_SETTINGS_MODULE default for scripts/distribute_reviews.bash

--- scripts/distribute_reviews.bash
+++ scripts/distribute_reviews.bash
@@ -4,7 +4,7 @@ LAST_DIR=$(pwd)
 cd /usr/src/app
 
 source /etc/environment
-DJANGO_SETTINGS_MODULE="${DJANGO_SETTINGS_MODULE:-mwrite_peer_review.settings.api}"
-python manage.py distribute_reviews
+DJANGO_SETTINGS_MODULE="${DJANGO_SETTINGS_MODULE:-mwrite_peer_review.settings.jobs}" \
+   python manage.py distribute_reviews
 
 cd ${LAST_DIR}
