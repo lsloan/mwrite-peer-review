dd965813ccaba46ca8a0dfabec039a1b7026e1b0
Dave Harlan <dkharlan@umich.edu>
2018-03-12 14:24:24 -0400

    Persist sections and students before returning data from all_students endpoint

--- peer_review/api/endpoints.py
+++ peer_review/api/endpoints.py
@@ -2,9 +2,9 @@ import logging
 
 from rolepermissions.roles import get_user_roles
 
+import peer_review.etl as etl
 from peer_review.models import CanvasStudent
 from peer_review.queries import ReviewDetails
-from peer_review.etl import persist_assignments
 from peer_review.api.util import merge_validations
 from peer_review.util import to_camel_case, keymap_all
 from peer_review.decorators import authorized_json_endpoint, authenticated_json_endpoint
@@ -25,13 +25,15 @@ def logged_in_user_details(request):
 
 @authorized_json_endpoint(roles=['instructor'])
 def all_students(request, course_id):
+    etl.persist_sections(course_id)
+    etl.persist_students(course_id)
     students = CanvasStudent.objects.filter(course_id=course_id)
     return [student.to_dict(levels=2) for student in students]
 
 
 @authorized_json_endpoint(roles=['instructor'])
 def all_peer_review_assignment_details(request, course_id):
-    assignments = persist_assignments(course_id)
+    assignments = etl.persist_assignments(course_id)
     fetched_assignment_ids = tuple(map(lambda a: a.id, assignments))
 
     details = ReviewDetails.get(course_id, fetched_assignment_ids)
