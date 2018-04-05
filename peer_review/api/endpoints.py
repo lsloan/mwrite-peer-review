7d6edd3627d0bf325deef9642712d41b203b5539
Dave Harlan <dkharlan@umich.edu>
2018-04-05 10:26:54 -0400

    Add endpoint for retrieving reviews given by a student

--- peer_review/api/endpoints.py
+++ peer_review/api/endpoints.py
@@ -1,10 +1,13 @@
 import logging
 
+from toolz.itertoolz import groupby
+from toolz.functoolz import thread_last
 from rolepermissions.roles import get_user_roles
+from django.db.models import Subquery
 from django.core.exceptions import PermissionDenied
 
 import peer_review.etl as etl
-from peer_review.models import CanvasStudent
+from peer_review.models import CanvasStudent, PeerReviewComment, Criterion
 from peer_review.queries import InstructorDashboardStatus, StudentDashboardStatus
 from peer_review.api.util import merge_validations
 from peer_review.util import to_camel_case, keymap_all
@@ -17,9 +20,11 @@ log = logging.getLogger(__name__)
 def logged_in_user_details(request):
     roles = [role.get_name() for role in get_user_roles(request.user)]
     course_id = request.session['lti_launch_params']['custom_canvas_course_id']
+    user_id = request.session['lti_launch_params']['custom_canvas_user_id']
     return {
         'username': request.user.username,
-        'course_id': course_id,
+        'user_id': int(user_id),
+        'course_id': int(course_id),
         'roles': roles
     }
 
@@ -64,3 +69,44 @@ def assigned_work(request, course_id, student_id):
 def completed_work(request, course_id, student_id):
     raise_if_not_current_user(request, student_id)
     return StudentDashboardStatus.completed_work(student_id)
+
+
+def _denormalize_comments(comments_qs):
+    comments_by_author_id = groupby(lambda c: c.peer_review.submission.author_id, comments_qs)
+
+    comments = []
+    for i, entry in enumerate(comments_by_author_id.items(), start=1):
+        author_id, comments_for_author = entry
+        data = {
+            'id': i,
+            'title': 'Student %d' % i,
+            'entries': [
+                {'id': comment.id,
+                 'heading': comment.criterion.description,
+                 'content': comment.comment}
+                for comment in comments_for_author
+            ]
+        }
+        comments.append(data)
+
+    return {
+        'title': comments_qs[0].peer_review.submission.assignment.title,
+        'entries': comments
+    }
+
+
+@authorized_json_endpoint(roles=['student'])
+def reviews_given(request, course_id, student_id, rubric_id):
+    comments = PeerReviewComment.objects.filter(
+        criterion_id__in=Subquery(
+            Criterion.objects.filter(rubric_id=rubric_id).values('id')
+        ),
+        peer_review__student_id=student_id,
+        peer_review__submission__assignment__course_id=course_id
+    ) \
+        .select_related('peer_review__student') \
+        .select_related('peer_review__submission') \
+        .select_related('criterion') \
+        .order_by('peer_review__student_id', 'peer_review__id', 'criterion_id')
+
+    return _denormalize_comments(comments)
