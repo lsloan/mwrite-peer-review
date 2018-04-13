117705d74bcb893463aa2ce78e11c71bd9e83c9c
Dave Harlan <dkharlan@umich.edu>
2018-04-13 15:04:18 -0400

    Style reviews by criteria view

--- frontend/src/components/ReviewsByCriterion.vue
+++ frontend/src/components/ReviewsByCriterion.vue
@@ -3,8 +3,8 @@
         <div class="criterion-entry" v-for="entry in data" :key="entry.id">
             <p class="criterion-name">{{ entry.title }}</p>
             <p class="criterion-description">{{ entry.criterion }}</p>
-            <p v-for="comment in entry.entries" :key="comment.id">
-                <span class="student-name">{{ comment.reviewerName }}</span>
+            <p class="student-review" v-for="comment in entry.entries" :key="comment.id">
+                <span class="student-name">{{ comment.reviewerName }}:</span>
                 <span>{{ comment.text }}</span>
             </p>
         </div>
@@ -14,12 +14,39 @@
 <script>
 export default {
   name: 'reviews-by-criterion',
-  props: ['data'],
-  mounted() {
-    console.log('reviews by criterion:', this.data);
-  }
+  props: ['data']
 };
 </script>
 
 <style scoped>
+    .criterion-entry {
+        padding-left: 16px;
+        border-left: 2px solid #4157AF;
+        margin-bottom: 20px;
+    }
+
+    .criterion-name, .criterion-description {
+        color: #4157AF;
+    }
+
+    .criterion-name {
+        margin-bottom: 0;
+        font-size: 14px;
+    }
+
+    .criterion-description {
+        margin-top: 0;
+    }
+
+    .criterion-description, .student-name {
+        font-size: 13px;
+    }
+
+    .student-name {
+        font-weight: bolder;
+    }
+
+    .student-review, .criterion-description {
+        margin-bottom: 4px;
+    }
 </style>
