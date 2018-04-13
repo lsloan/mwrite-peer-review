af0f3dce1d2afd3cabada99a37eaf695d89a4868
Dave Harlan <dkharlan@umich.edu>
2018-04-13 10:30:17 -0400

    Add a component for viewing peer review comments sorted by criterion

---
+++ frontend/src/components/ReviewsByCriterion.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="criterion-entry" v-for="entry in data" :key="entry.id">
+            <p class="criterion-name">{{ entry.title }}</p>
+            <p class="criterion-description">{{ entry.criterion }}</p>
+            <p v-for="comment in entry.entries" :key="comment.id">
+                <span class="student-name">{{ comment.reviewerName }}</span>
+                <span>{{ comment.text }}</span>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'reviews-by-criterion',
+  props: ['data'],
+  mounted() {
+    console.log('reviews by criterion:', this.data);
+  }
+};
+</script>
+
+<style scoped>
+</style>
