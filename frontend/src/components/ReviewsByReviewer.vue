91cf98ed485fb14696aac0e710ffdfac7fc3995c
Dave Harlan <dkharlan@umich.edu>
2018-04-16 14:08:22 -0400

    Implement (unstyled) backeval UI (without actual POST)

--- frontend/src/components/ReviewsByReviewer.vue
+++ frontend/src/components/ReviewsByReviewer.vue
@@ -1,25 +1,80 @@
 <template>
-    <mdl-tabs v-model="selectedTab" class="tabs">
-        <mdl-tab v-for="entry in data" :key="entry.id" :tab="entry.title">
-            <div v-for="subEntry in entry.entries" :key="subEntry.id">
-                <p class="heading">{{ subEntry.heading }}</p>
-                <p>{{ subEntry.content }}</p>
+    <div>
+        <mdl-tabs v-model="selectedTab" class="tabs">
+            <mdl-tab v-for="entry in data" :key="entry.id" :tab="entry.title">
+                <div v-for="subEntry in entry.entries" :key="subEntry.id">
+                    <p class="heading">{{ subEntry.heading }}</p>
+                    <p>{{ subEntry.content }}</p>
+                </div>
+            </mdl-tab>
+        </mdl-tabs>
+
+        <div v-if="!showEvaluation">
+            <div v-if="!selectedEvaluationSubmitted">
+                <button @click="showEvaluation = true">Rate This Evaluation</button>
             </div>
-        </mdl-tab>
-    </mdl-tabs>
+            <div v-else>
+                <p>Submitted</p>
+            </div>
+        </div>
+        <div v-else>
+            <label>
+                Please rate the overall usefulness of this review
+                <mdl-radio v-model="usefulness" val="1">Very unuseful</mdl-radio>
+                <mdl-radio v-model="usefulness" val="2">Unuseful</mdl-radio>
+                <mdl-radio v-model="usefulness" val="3">Somewhat useful</mdl-radio>
+                <mdl-radio v-model="usefulness" val="4">Useful</mdl-radio>
+                <mdl-radio v-model="usefulness" val="5">Very useful</mdl-radio>
+            </label>
+            <mdl-textfield
+                v-model="evaluationComment"
+                label="Please provide any additional feedback on this review"/>
+            <button @click="submitEvaluation">Submit</button>
+            <button @click="showEvaluation = false">Cancel</button>
+        </div>
+
+    </div>
 </template>
 
 <script>
-import {MdlTab, MdlTabs} from 'vue-mdl';
+import {MdlTab, MdlTabs, MdlRadio} from 'vue-mdl';
 
 export default {
   name: 'reviews-by-reviewer',
   props: ['data'],
-  components: {MdlTab, MdlTabs},
+  components: {MdlTab, MdlTabs, MdlRadio},
   data() {
     return {
-      selectedTab: ''
+      selectedTab: '',
+      showEvaluation: false,
+      usefulness: null,
+      evaluationComment: null
     };
+  },
+  computed: {
+    selectedPeerReviewId() {
+      if(this.data) {
+        const entry = this.data.find(e => e.title === this.selectedTab);
+        return entry.peerReviewId;
+      }
+    },
+    selectedEvaluationSubmitted() {
+      if(this.data) {
+        const entry = this.data.find(e => e.title === this.selectedTab);
+        return entry.evaluationSubmitted;
+      }
+    }
+  },
+  methods: {
+    submitEvaluation() {
+      console.log(
+        'would have submitted',
+        this.usefulness,
+        'and',
+        this.evaluationComment,
+        'for', this.selectedPeerReviewId
+      );
+    }
   },
   watch: {
     data() {
