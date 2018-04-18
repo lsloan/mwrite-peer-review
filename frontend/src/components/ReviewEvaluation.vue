00b61acc0669374a433ebd48676c81b020a47eff
Dave Harlan <dkharlan@umich.edu>
2018-04-18 11:35:02 -0400

    More progress towards styling evaluation form

--- frontend/src/components/ReviewEvaluation.vue
+++ frontend/src/components/ReviewEvaluation.vue
@@ -7,21 +7,30 @@
             <div v-if="!showEvaluation">
                 <button class="evaluation-button" @click="showEvaluation = true">Rate This Evaluation</button>
             </div>
-            <div v-else>
-                <label>
-                    Please rate the overall usefulness of this review
-                    <mdl-radio v-model="usefulness" val="1">Very unuseful</mdl-radio>
-                    <mdl-radio v-model="usefulness" val="2">Unuseful</mdl-radio>
-                    <mdl-radio v-model="usefulness" val="3">Somewhat useful</mdl-radio>
-                    <mdl-radio v-model="usefulness" val="4">Useful</mdl-radio>
-                    <mdl-radio v-model="usefulness" val="5">Very useful</mdl-radio>
-                </label>
-                <mdl-textfield
-                    v-model="evaluationComment"
-                    label="Please provide any additional feedback on this review"/>
-                <button @click="submitEvaluation(entry)">Submit</button>
-                <button @click="showEvaluation = false">Cancel</button>
-            </div>
+            <form v-else class="evaluation-card" v-on:submit.prevent="submitEvaluation(entry)">
+                <div class="form-section">
+                    <fieldset class="usefulness-fields">
+                        <legend class="form-label">Please rate the overall usefulness of this review</legend>
+                        <mdl-radio v-model="usefulness" val="1">Very unuseful</mdl-radio>
+                        <mdl-radio v-model="usefulness" val="2">Unuseful</mdl-radio>
+                        <mdl-radio v-model="usefulness" val="3">Somewhat useful</mdl-radio>
+                        <mdl-radio v-model="usefulness" val="4">Useful</mdl-radio>
+                        <mdl-radio v-model="usefulness" val="5">Very useful</mdl-radio>
+                    </fieldset>
+                </div>
+                <div class="form-section">
+                    <label>
+                        <span class="form-label">Please provide any additional feedback on this review</span>
+                        <mdl-textfield
+                            class="feedback-input"
+                            v-model="evaluationComment"/>
+                    </label>
+                </div>
+                <div class="form-section">
+                    <input type="submit" value="Submit"/>
+                    <button @click="showEvaluation = false">Cancel</button>
+                </div>
+            </form>
         </div>
     </div>
 </template>
@@ -66,6 +75,35 @@ export default {
 <style scoped>
     .evaluation-container {
         margin-top: 35px;
+    }
+
+    .evaluation-card {
+        border: 1px solid lightgray;
+        border-top: 4px solid #4157AF;
+        padding: 8px 20px;
+    }
+
+    .form-section {
+        margin-bottom: 15px;
+    }
+
+    .form-label {
+        font-weight: bolder;
+        display: block;
+        margin-bottom: 8px;
+    }
+
+    .usefulness-fields {
+        margin: 0 10px;
+    }
+
+    .usefulness-fields > legend {
+        margin: 0 -10px 8px -10px;
+    }
+
+    .feedback-input {
+        width: 100%;
+        padding: 0px;
     }
 
     .evaluation-button {
