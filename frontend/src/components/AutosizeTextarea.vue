87a8e36a2724e5e08c00e5f8697bc212376bad13
Dave Harlan <dkharlan@umich.edu>
2018-05-15 13:58:13 -0400

    Convert AutosizeTextarea component to single file component format

---
+++ frontend/src/components/AutosizeTextarea.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="mdl-textfield mdl-js-textfield">
+        <textarea
+            :id="_uid"
+            class="autosize-textarea mdl-textfield__input"
+            type="text"
+            rows="1"
+            :disabled="disabled"
+            @input="$emit('input', $event.target.value)"
+            v-model="value"/>
+    <label :for="_uid" class="mdl-textfield__label">{{ label }}</label> +
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'autosize-textarea',
+  props: ['value', 'disabled']
+};
+</script>
+
+<style scoped>
+</style>
