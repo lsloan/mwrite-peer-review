9a2920438e0c94abeb8bf7761e17b842d94f0a97
Dave Harlan <dkharlan@umich.edu>
2018-05-24 15:02:59 -0400

    Adjust responsive cell breaks

--- frontend/src/components/PeerReviewSection.vue
+++ frontend/src/components/PeerReviewSection.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="mdl-grid">
-        <div class="mdl-cell mdl-cell--2-col" v-if="sectionNumber"></div>
-        <div class="mdl-cell mdl-cell--1-col" v-if="sectionNumber">
+        <div class="mdl-cell mdl-cell--2-col mdl-cell--hide-tablet mdl-cell--hide-phone" v-if="sectionNumber"></div>
+        <div class="mdl-cell mdl-cell--1-col mdl-cell--1-col-tablet mdl-cell--hide-phone" v-if="sectionNumber">
             <aside class="section-number-container">
                 <p class="section-number">{{ sectionNumber }}</p>
             </aside>
         </div>
-        <div class="mdl-cell mdl-cell--3-col" v-else></div>
-        <section class="mdl-cell mdl-cell--6-col">
+        <div class="mdl-cell mdl-cell--3-col mdl-cell--1-col-tablet mdl-cell--hide-phone" v-else></div>
+        <section class="mdl-cell mdl-cell--6-col mdl-cell--6-col-tablet mdl-cell--4-col-phone">
             <slot></slot>
         </section>
-        <div class="mdl-cell mdl-cell--3-col"></div>
+        <div class="mdl-cell mdl-cell--3-col mdl-cell--1-col-table mdl-cell--hide-phone"></div>
     </div>
 </template>
 
