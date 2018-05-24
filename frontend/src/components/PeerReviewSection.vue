5c9a205b920de18ab95f004a2e685dc160a58c28
Dave Harlan <dkharlan@umich.edu>
2018-05-24 09:59:45 -0400

    Add PeerReviewSection component to reduce boilerplate and refactor

---
+++ frontend/src/components/PeerReviewSection.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="mdl-grid">
+        <div class="mdl-cell mdl-cell--2-col" v-if="sectionNumber"></div>
+        <div class="mdl-cell mdl-cell--1-col" v-if="sectionNumber">
+            <aside class="section-number-container">
+                <p class="section-number">{{ sectionNumber }}</p>
+            </aside>
+        </div>
+        <div class="mdl-cell mdl-cell--3-col" v-else></div>
+        <section class="mdl-cell mdl-cell--6-col">
+            <slot></slot>
+        </section>
+        <div class="mdl-cell mdl-cell--3-col"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'PeerReviewSection',
+  props: ['section-number']
+};
+</script>
+
+<style scoped>
+    .section-number-container {
+        display: flex;
+        flex-direction: row-reverse;
+    }
+
+    .section-number {
+        text-align: center;
+        color: white;
+        background: #4187CD;
+        width: 36px;
+        height: 36px;
+        line-height: 36px;
+        -moz-border-radius: 18px;
+        -webkit-border-radius: 18px;
+        border-radius: 18px;
+    }
+</style>
--- frontend/src/pages/PeerReview.vue
+++ frontend/src/pages/PeerReview.vue
@@ -1,37 +1,21 @@
 <template>
     <div>
-        <div class="mdl-grid">
-            <div class="mdl-cell mdl-cell--1-col">
-                <aside class="section-number-container">
-                    <p class="section-number">1</p>
-                </aside>
-            </div>
-            <div class="mdl-cell mdl-cell--10-col">
-                <a :href="submissionDownloadUrl"
-                   class="mdl-button mdl-button--raised mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-                   Download Submission
-                </a>
-            </div>
-            <div class="mdl-cell mdl-cell--1-col"></div>
-        </div>
-        <div class="mdl-grid">
-            <div class="mdl-cell mdl-cell--1-col">
-                <aside class="section-number-container">
-                    <p class="section-number">2</p>
-                </aside>
-            </div>
-            <div class="mdl-cell mdl-cell--10-col">
-                <h1>Submit Your Review</h1>
-            </div>
-            <div class="mdl-cell mdl-cell--1-col"></div>
-        </div>
+        <peer-review-section section-number="1">
+            <a :href="submissionDownloadUrl"
+                class="mdl-button mdl-button--raised mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                Download Submission
+            </a>
+        </peer-review-section>
     </div>
 </template>
 
 <script>
+import PeerReviewSection from '@/components/PeerReviewSection';
+
 export default {
   name: 'PeerReview',
   props: ['review-id'],
+  components: {PeerReviewSection},
   computed: {
     submissionDownloadUrl() {
       const {courseId} = this.$store.state.userDetails;
@@ -42,23 +26,6 @@ export default {
 </script>
 
 <style scoped>
-    .section-number-container {
-        display: flex;
-        flex-direction: row-reverse;
-    }
-
-    .section-number {
-        text-align: center;
-        color: white;
-        background: #4187CD;
-        width: 36px;
-        height: 36px;
-        line-height: 36px;
-        -moz-border-radius: 18px;
-        -webkit-border-radius: 18px;
-        border-radius: 18px;
-    }
-
     h1 {
         font-size: 24px;
         line-height: 24px;
