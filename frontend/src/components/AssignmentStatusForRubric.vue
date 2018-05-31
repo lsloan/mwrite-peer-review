dbac26f945a76c5f8aa404059835785565056c0e
Dave Harlan <dkharlan@umich.edu>
2018-05-31 11:26:28 -0400

    Add layout for rubric detail page

--- frontend/src/components/AssignmentStatusForRubric.vue
+++ frontend/src/components/AssignmentStatusForRubric.vue
@@ -1,13 +1,45 @@
 <template>
-    <div>assignment status overview for {{ studentId }} and {{ rubricId }} goes here</div>
+    <div class="mdl-grid">
+        <div class="mdl-cell mdl-cell--6-col">
+            <div class="mdl-grid">
+                <div class="mdl-cell mdl-cell--12-col"><h1>Reviews Completed</h1></div>
+            </div>
+            <div class="mdl-grid">
+                <peer-review-status-card v-for="(_, index) in reviewsToBeCompleted" :key="index"/>
+            </div>
+        </div>
+        <div class="mdl-cell mdl-cell--6-col">
+            <div class="mdl-grid">
+                <div class="mdl-cell mdl-cell--12-col"><h1>Reviews Received</h1></div>
+            </div>
+            <div class="mdl-grid">
+                <peer-review-status-card v-for="(_, index) in reviewsToBeReceived" :key="index"/>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import PeerReviewStatusCard from '@/components/PeerReviewStatusCard';
+
 export default {
   name: 'AssignmentStatusForRubric',
-  props: ['student-id', 'rubric-id']
+  props: ['student-id', 'rubric-id'],
+  components: {PeerReviewStatusCard},
+  data() {
+    return {
+      reviewsToBeCompleted: [1, 2, 3],
+      reviewsToBeReceived: [1, 2, 3]
+    };
+  }
 };
 </script>
 
 <style scoped>
+    h1 {
+        font-family: "Roboto","Helvetica","Arial",sans-serif;
+        font-size: 28px;
+        line-height: 28px;
+        margin: 8px 0;
+    }
 </style>
---
+++ frontend/src/components/PeerReviewStatusCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="mdl-card mdl-shadow--2dp mdl-cell mdl-cell--12-col">
+        <div class="mdl-card__title">
+            Howdy
+        </div>
+        <div class="mdl-card__supporting-text">
+            Here's some text
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'PeerReviewStatusCard'
+};
+</script>
+
+<style scoped>
+</style>
