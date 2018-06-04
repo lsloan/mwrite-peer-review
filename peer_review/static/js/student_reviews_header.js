f902f344f498e6c8cab0fb283d88963210ead2b4
Dave Harlan <dkharlan@umich.edu>
2018-06-04 15:59:24 -0400

    Remove legacy views and static resources, consolidate Safari launch template

--- peer_review/static/js/student_reviews_header.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $('.each_review').each(function() {
-        if($(this).find(".each_review_link").attr('href') === window.location.href.replace(window.location.origin, '')) {
-            $(this).addClass("show_border");
-            $(this).find(".each_review_link").addClass("change_link_style");
-        }
-    });
-});
