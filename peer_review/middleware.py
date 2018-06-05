cec7c31ca3e92b4d63b29326f834573c89e6043f
Dave Harlan <dkharlan@umich.edu>
2018-06-05 11:02:45 -0400

    Reorder Safari launch middleware since we no longer have the course ID on the URL

--- peer_review/middleware.py
+++ peer_review/middleware.py
@@ -16,21 +16,21 @@ class FixedUserAgentMiddleware(MiddlewareMixin, UserAgentMiddleware):
 def safari_iframe_launch_middleware(get_response):
 
     def middleware(request):
-        url_match = re.search('^/launch$', request.path)
-        browser_is_safari = request.user_agent.browser.family == 'Safari'
-        safari_cookie_missing = settings.SAFARI_LAUNCH_COOKIE not in request.COOKIES
-        have_launch_params = 'lti_launch_params' in request.session
+        response = get_response(request)
 
-        if url_match and browser_is_safari and safari_cookie_missing  and have_launch_params:
-            LOGGER.debug('Unauthenticated Safari user detected, serving Safari landing page')
-            context = {
-                'referer': request.META['HTTP_REFERER'],
-                'course_id': request.session['lti_launch_params']['context_id']
-            }
-            return render_to_response('safari_launch_iframe.html', context=context)
-        else:
-            response = get_response(request)
-            LOGGER.debug('launch params = %s', request.session.get('lti_launch_params'))
+        if request.user_agent.browser.family == 'Safari' and \
+              settings.SAFARI_LAUNCH_COOKIE not in request.COOKIES:
+            url_match = re.search('^/launch$', request.path)
+            have_launch_params = 'lti_launch_params' in request.session
+            
+            if url_match and have_launch_params:
+                LOGGER.debug('Unauthenticated Safari user detected, serving Safari landing page')
+                context = {
+                    'referer': request.META['HTTP_REFERER'],
+                    'course_id': request.session['lti_launch_params']['context_id']
+                }
+                response = render_to_response('safari_launch_iframe.html', context=context)
+
         return response
 
     return middleware
