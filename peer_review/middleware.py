676acbb752b22f04159c70f42c5a155e37851969
Dave Harlan <dkharlan@umich.edu>
2018-06-05 09:55:55 -0400

    Add some debug logging to Safari launch middleware

--- peer_review/middleware.py
+++ peer_review/middleware.py
@@ -5,7 +5,7 @@ from django.shortcuts import render_to_response
 from django.utils.deprecation import MiddlewareMixin
 from django_user_agents.middleware import UserAgentMiddleware
 
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 
 # TODO remove when django-user-agents fixes https://github.com/selwin/django-user_agents/issues/13
@@ -16,12 +16,17 @@ class FixedUserAgentMiddleware(MiddlewareMixin, UserAgentMiddleware):
 def safari_iframe_launch_middleware(get_response):
 
     def middleware(request):
+        LOGGER.debug('request path = %s, browser family = %s',
+                     request.path, request.user_agent.browser.family)
+
         url_match = re.search('^launch$', request.path)
         browser_is_safari = request.user_agent.browser.family == 'Safari'
         safari_cookie_exists = settings.SAFARI_LAUNCH_COOKIE not in request.COOKIES
 
+        LOGGER.debug('safar cookie exists = %s', safari_cookie_exists)
+
         if url_match and browser_is_safari and safari_cookie_exists:
-            logger.debug('Unauthenticated Safari user detected, serving Safari landing page')
+            LOGGER.debug('Unauthenticated Safari user detected, serving Safari landing page')
             context = {
                 'referer': request.META['HTTP_REFERER'],
                 'course_id': request.session['lti_launch_params']['context_id']
