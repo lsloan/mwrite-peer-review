7767d81e90b0e251e588aca87311e6dfa9079099
Dave Harlan <dkharlan@umich.edu>
2018-07-11 13:35:32 -0400

    Update the textarea element after the DOM is updated when setting AutosizeTextarea's value prop

--- frontend/src/components/AutosizeTextarea.vue
+++ frontend/src/components/AutosizeTextarea.vue
@@ -2,12 +2,13 @@
     <div class="mdl-textfield mdl-js-textfield">
         <textarea
             :id="_uid"
+            ref="textarea"
             class="autosize-textarea mdl-textfield__input"
-            type="text"
             rows="1"
             :disabled="disabled"
             @input="$emit('input', $event.target.value)"
-            :value="value"/>
+            :value="value">
+        </textarea>
     <label v-if="label" :for="_uid" class="mdl-textfield__label">{{ label }}</label>
     </div>
 </template>
@@ -19,11 +20,19 @@ export default {
   name: 'autosize-textarea',
   props: ['value', 'disabled', 'label'],
   mounted() {
-    autosize(this.$el.querySelector('textarea'));
+    autosize(this.$refs.textarea);
     componentHandler.upgradeElement(this.$el); // eslint-disable-line no-undef
   },
   updated() {
     this.$el.MaterialTextfield.checkDirty();
+  },
+  watch: {
+    value() {
+      this.$nextTick(() => {
+        // see https://github.com/jackmoore/autosize/issues/364
+        autosize.update(this.$refs.textarea);
+      });
+    }
   }
 };
 </script>
