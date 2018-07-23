0d89e1366efb3b28d351586bfbb061752d0f6806
Dave Harlan <dkharlan@umich.edu>
2018-07-23 16:00:39 -0400

    Move byDateAscending comparator to services/util

--- frontend/src/services/util.js
+++ frontend/src/services/util.js
@@ -1,6 +1,14 @@
+import moment from 'moment';
+
 const lastSymbolTable = {};
 
 export const gensym = (prefix = 'symbol') => {
   lastSymbolTable[prefix] = lastSymbolTable[prefix] + 1 || 0;
   return `${prefix}${lastSymbolTable[prefix]}`;
+};
+
+export const byDateAscending = (a, b) => {
+  const dateA = moment.utc(a.dueDateUtc);
+  const dateB = moment.utc(b.dueDateUtc);
+  return dateA.diff(dateB);
 };
