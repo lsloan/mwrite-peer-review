3c9b74bebebb59a31209d73b64d3538f0820c5b6
Dave Harlan <dkharlan@umich.edu>
2018-07-23 14:29:49 -0400

    Implement a new wrapper component for mdl-snackbar so that we can use role="alert" on the text contanier

---
+++ frontend/src/components/Snackbar.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="mdl-snackbar mdl-js-snackbar"
+         aria-live="assertive"
+         aria-atomic="true"
+         aria-relevant="text">
+        <div class="mdl-snackbar__text" role="alert"></div>
+        <button class="mdl-snackbar__action" type="button"></button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Snackbar',
+  props: {
+    displayOn: {
+      type: String,
+      required: true
+    },
+    eventSource: {
+      type: Object,
+      required: false,
+      default() {
+        return this.$root;
+      }
+    }
+  },
+  methods: {
+    showSnackbar(config) {
+      this.$el.MaterialSnackbar.showSnackbar(config);
+    }
+  },
+  mounted() {
+    componentHandler.upgradeElement(this.$el, 'MaterialSnackbar'); // eslint-disable-line no-undef
+    this.eventSource.$on(this.displayOn, this.showSnackbar);
+  },
+  destroyed() {
+    this.eventSource.$off(this.displayOn, this.showSnackbar);
+  }
+};
+</script>
+
+<style scoped>
+</style>
