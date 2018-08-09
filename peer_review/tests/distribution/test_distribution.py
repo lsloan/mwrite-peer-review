968ffb8a3285e711783af4d5f842a475956d898f
Dave Harlan <dkharlan@umich.edu>
2018-08-09 11:06:26 -0400

    Refactor distribution tests to use mocked models and requests rather than integration with a real Canvas instance; split old integration code out to separate util package

--- peer_review/tests/distribution/test_distribution.py
+++ peer_review/tests/distribution/test_distribution.py
@@ -5,21 +5,14 @@ from statistics import mean, stdev
 from hypothesis import given
 from hypothesis.strategies import integers
 
-import peer_review.tests.canvas.fixtures as f
+from peer_review.tests.canvas.fixtures import test_models, rubric_tree_with_mocked_requests
 from peer_review.models import CanvasStudent, CanvasSubmission, PeerReview
 from peer_review.distribution import make_distribution, review_distribution_task
 from .strategies import students_and_submissions
 
 
-# noinspection PyUnusedLocal
-@pytest.fixture
-def canvas_integration_rubrics(transactional_db):
-    # TODO need to find a way to not need to hard code these. config files?
-    return f.canvas_integration_course(15, [108, 109, 129])
-
-
 @given(sns=students_and_submissions(), n=integers(min_value=1, max_value=5))
-def test_make_distribution(sns, n):
+def test_distribution(sns, n):
     students, submissions = sns
     reviews, counts = make_distribution(students, submissions, n)
     for student_id, submissions_to_review in reviews.items():
@@ -31,19 +24,17 @@ def test_make_distribution(sns, n):
     assert s < 0.25
 
 
-# noinspection PyShadowingNames,PyUnusedLocal
+# noinspection PyShadowingNames
 @pytest.mark.django_db(True)
-def test_distribution_task(canvas_integration_rubrics):
-    test_rubric = canvas_integration_rubrics[0]
+def test_distribution_task_for_all_sections(rubric_tree_with_mocked_requests):
+    rubric = rubric_tree_with_mocked_requests
 
     review_distribution_task(datetime.utcnow(), True)
 
-    assert test_rubric.peer_review_distribution.is_distribution_complete
+    assert rubric.peer_review_distribution.is_distribution_complete
 
     # each submission should have at least one reviewer
-    submissions = CanvasSubmission.objects.filter(
-        id__in=test_rubric.reviewed_assignment.canvas_submission_set.values_list('id', flat=True)
-    )
+    submissions = rubric.reviewed_assignment.canvas_submission_set.all()
     for submission in submissions:
         assert PeerReview.objects.filter(submission_id=submission.id).exists()
 
@@ -52,7 +43,7 @@ def test_distribution_task(canvas_integration_rubrics):
     )
     for author in authors:
         # each student should have at least one review for the prompt
-        submissions_for_prompt = CanvasSubmission.objects.filter(assignment_id=test_rubric.reviewed_assignment)
+        submissions_for_prompt = CanvasSubmission.objects.filter(assignment_id=rubric.reviewed_assignment)
         submission_ids_for_prompt = submissions_for_prompt.values_list('id', flat=True)
         peer_reviews_for_student = PeerReview.objects.filter(
             student=author,
@@ -65,16 +56,29 @@ def test_distribution_task(canvas_integration_rubrics):
         assert peer_reviews_for_student.count() == submission_ids_for_peer_review.count()
 
         # students should not be reviewing their own submission
-        own_submission = CanvasSubmission.objects.get(assignment=test_rubric.reviewed_assignment, author=author)
+        own_submission = CanvasSubmission.objects.get(assignment=rubric.reviewed_assignment, author=author)
         assert own_submission.id not in submission_ids_for_peer_review
 
-        # each student should only be reviewing submissions from his/her section
-        author_sections = author.sections.filter(id__in=test_rubric.sections.values_list('id', flat=True))
+
+# noinspection PyShadowingNames
+@pytest.mark.skip(reason='Section-only distribution is currently unused')
+@pytest.mark.django_db(True)
+def test_distribution_task_within_sections(rubric_tree_with_mocked_requests):
+    rubric = rubric_tree_with_mocked_requests
+
+    review_distribution_task(datetime.utcnow(), True)
+
+    submissions = rubric.reviewed_assignment.canvas_submission_set.all()
+    authors = CanvasStudent.objects.filter(id__in=submissions.values_list('author_id', flat=True))
+
+    # each student should only be reviewing submissions from his/her section
+    for author in authors:
+        author_sections = author.sections.filter(id__in=rubric.sections.values_list('id', flat=True))
         assert len(author_sections) == 1
-        other_section_ids = test_rubric.sections.exclude(id=author_sections[0].id).values_list('id', flat=True)
+        other_section_ids = rubric.sections.exclude(id=author_sections[0].id).values_list('id', flat=True)
         other_section_student_ids = CanvasStudent.objects.filter(sections__id__in=other_section_ids) \
             .values_list('id', flat=True)
-        submissions_not_to_review = submissions_for_prompt.filter(author_id__in=other_section_student_ids)
+        submissions_not_to_review = submissions.filter(author_id__in=other_section_student_ids)
         erroneous_peer_reviews = PeerReview.objects.filter(
             student=author,
             submission_id__in=submissions_not_to_review.values_list('id', flat=True)
