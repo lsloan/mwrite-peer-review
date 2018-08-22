83fe894a6066623ea95de715dd0cdc41eea29fe9
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2018-08-22 17:26:10 -0400

    origin/280-inst-eval-view - Keep vertical spacing
    To keep the same vertical spacing on review assignment cards, move the `mdl-card--expand` class from the title element to a new empty element just above the action buttons.

--- frontend/src/components/PeerReviewAssignmentCard.vue
+++ frontend/src/components/PeerReviewAssignmentCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="peer-review-assignment-card mdl-card mdl-cell mdl-cell--3-col mdl-shadow--2dp">
 
-        <div class="mdl-card__title mdl-card--expand">
+        <div class="mdl-card__title">
             <!-- TODO should be an h1? -->
             <h2 class="mdl-card__title-text">{{ peerReviewTitle }}</h2>
         </div>
@@ -60,12 +60,14 @@
             </div>
         </div>
 
-        <div class="mdl-card__supporting-text" v-if="reviewsInProgress">
+        <div class="mdl-card__supporting-text" v-if="evaluationDueDate">
             <div class="icon-container">
                 <i class="material-icons icon-24px">alarm</i>
                 <span class="icon-caption">Evaluations due by {{ evaluationDueDate | utcToLocal(dateFormat) }}</span>
             </div>
         </div>
+
+        <div class="mdl-card--expand"></div>
 
         <div class="mdl-card__actions mdl-card--border">
             <router-link class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
@@ -83,57 +85,57 @@
 </template>
 
 <script>
-import {validationInfoAsIssues} from '@/services/validation';
-import DateFormat from '@/mixins/date-format';
+    import {validationInfoAsIssues} from '@/services/validation';
+    import DateFormat from '@/mixins/date-format';
 
-export default {
-  name: 'peer-review-assignment-card',
-  props: [
-    'rubric-id',
-    'reviews-in-progress',
-    'due-date',
-    'open-date',
-    'evaluation-due-date',
-    'peer-review-assignment-id',
-    'peer-review-title',
-    'date-format',
-    'validation-info',
-    'number-of-assigned-reviews',
-    'number-of-completed-reviews'
-  ],
-  mixins: [DateFormat],
-  computed: {
-    rubricActionText() {
-      return this.reviewsInProgress
-        ? 'View'
-        : (this.rubricId ? 'Edit' : 'Create');
-    },
-    courseId() {
-      return this.$store.state.userDetails.courseId;
-    },
-    issues() {
-      return validationInfoAsIssues(this.validationInfo);
-    },
-    rubricExists() {
-      return Boolean(this.rubricId);
-    },
-    rubricHasFatalIssues() {
-      return this.issues.length > 0 && this.issues.some(i => i.fatal);
-    },
-    rubricHasNonFatalIssues() {
-      return this.issues.length > 0 && this.issues.some(i => !i.fatal);
-    },
-    validationIconClasses() {
-      return {
-        'material-icons': true,
-        'icon-24px': true,
-        'icon-color-ok': this.rubricExists && this.issues.length === 0,
-        'icon-color-warning': this.rubricExists && this.rubricHasNonFatalIssues,
-        'icon-color-error': this.rubricExists && this.rubricHasFatalIssues
-      };
-    }
-  }
-};
+    export default {
+        name: 'peer-review-assignment-card',
+        props: [
+            'rubric-id',
+            'reviews-in-progress',
+            'due-date',
+            'open-date',
+            'evaluation-due-date',
+            'peer-review-assignment-id',
+            'peer-review-title',
+            'date-format',
+            'validation-info',
+            'number-of-assigned-reviews',
+            'number-of-completed-reviews'
+        ],
+        mixins: [DateFormat],
+        computed: {
+            rubricActionText() {
+                return this.reviewsInProgress
+                    ? 'View'
+                    : (this.rubricId ? 'Edit' : 'Create');
+            },
+            courseId() {
+                return this.$store.state.userDetails.courseId;
+            },
+            issues() {
+                return validationInfoAsIssues(this.validationInfo);
+            },
+            rubricExists() {
+                return Boolean(this.rubricId);
+            },
+            rubricHasFatalIssues() {
+                return this.issues.length > 0 && this.issues.some(i => i.fatal);
+            },
+            rubricHasNonFatalIssues() {
+                return this.issues.length > 0 && this.issues.some(i => !i.fatal);
+            },
+            validationIconClasses() {
+                return {
+                    'material-icons': true,
+                    'icon-24px': true,
+                    'icon-color-ok': this.rubricExists && this.issues.length === 0,
+                    'icon-color-warning': this.rubricExists && this.rubricHasNonFatalIssues,
+                    'icon-color-error': this.rubricExists && this.rubricHasFatalIssues
+                };
+            }
+        }
+    };
 </script>
 
 <style scoped>
