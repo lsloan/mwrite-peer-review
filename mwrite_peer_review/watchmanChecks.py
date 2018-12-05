c6729e4b580ef86f1a469d823a54898a03ecb5f0
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2018-12-05 17:13:38 -0500

    #384 - Support for Nagios
    Improved checks with fault tolerance.

--- mwrite_peer_review/watchmanChecks.py
+++ mwrite_peer_review/watchmanChecks.py
@@ -11,17 +11,31 @@ def jobsCheck() -> dict:
     jobIntervalMinutes: int = 15
     jobIntervalToleranceMinutes: int = 2
     jobIntervalSeconds: int = (jobIntervalMinutes + jobIntervalToleranceMinutes) * 60
-
     now: datetime = datetime.now(tzutc())
+
+    latestTimestamp: str = None
+    latestIntervalSeconds: float = None
+    latestIntervalOk: bool = False
+
     latestMessage: JobLog = JobLog.objects.order_by('-' + JobLog.timestamp.field_name).first()
 
-    latestIntervalSeconds: int = (now - latestMessage.timestamp).total_seconds()
+    if (latestMessage is not None):
+        latestTimestamp = latestMessage.timestamp.isoformat()
+        latestIntervalSeconds = (now - latestMessage.timestamp).total_seconds()
+        latestIntervalOk = latestIntervalSeconds < jobIntervalSeconds
+
+    jobsResult = {
+        'latestTimestamp': latestTimestamp,
+        'latestIntervalSeconds': latestIntervalSeconds,
+        'latestIntervalOk': latestIntervalOk,
+        'ok': latestIntervalOk,  # watchman requires "ok"; undocumented
+    }
+
+    if (not latestTimestamp):
+        jobsResult['errorMessage'] = 'Unable to find a timestamp for the latest job run.'
+    elif (not latestIntervalOk):
+        jobsResult['errorMessage'] = 'Latest job run is not within the past {} minutes.'.format(jobIntervalMinutes)
 
     return {
-        'jobs': {
-            'latestTimestamp': latestMessage.timestamp.isoformat(),
-            'latestIntervalSeconds': latestIntervalSeconds,
-            'latestIntervalOk': latestIntervalSeconds < jobIntervalSeconds,
-            'ok': latestIntervalSeconds < jobIntervalSeconds,  # watchman requires "ok"; undocumented; bug?
-        }
+        'jobs': jobsResult,
     }
