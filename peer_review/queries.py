8b1386e48a70944dd0ef97b0fa0df4c5ac3278b1
Lance E Sloan (lsloan) <lsloan@umich.edu>
2020-01-13 11:56:01 -0500

    #256 - date comparison
    On test server, MPR complains that it can't compare None due date.  Strangely, all the assignments involved in my test have due dates.  Maybe this comparison is being applied to old assignments that don't have due dates.  Trying new logic to pass those through as "null" and I will query the API to see if that's the case.

--- peer_review/queries.py
+++ peer_review/queries.py
@@ -130,11 +130,19 @@ class StudentDashboardStatus:
         prompt_id, peer_reviews = entry
         prompt_name = peer_reviews[0].submission.assignment.title
         due_date_utc: datetime = peer_reviews[0].submission.assignment.rubric_for_prompt.passback_assignment.due_date_utc
+
+        due_date_has_passed: bool = None
+
+        try:
+            due_date_has_passed = due_date_utc < datetime.now(pytz.UTC)
+        except:
+            pass
+
         return {
             'prompt_id':    prompt_id,
             'prompt_name':  prompt_name,
             'due_date_utc': due_date_utc,
-            'due_date_has_passed': due_date_utc < datetime.now(pytz.UTC),
+            'due_date_has_passed': due_date_has_passed,
         }
 
     @staticmethod
